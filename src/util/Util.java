fccdd2935bb86da8c6882389dadb32ae9ee5e954
Rajarshi Das <rajarshd@cs.cmu.edu>
2013-07-28 23:24:54 -0400

    checking in la4j jar

---
+++ src/util/Util.java
@@ -0,0 +1,21 @@
+package util;
+
+public class Util {
+
+	//path to the distances file
+	private static final String distanceFile = "Data/distances.txt";
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void createDistanceMatrix()
+	{
+		
+	}
+
+}
